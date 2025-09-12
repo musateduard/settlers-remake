8b90d72576db0427dce1598767141ef8199c5901
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-09-13 01:29:30 +0200

    updated OriginalMainMenu.java

--- jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/OriginalCampaignMenu.java
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/OriginalCampaignMenu.java
@@ -3,22 +3,16 @@ package jsettlers.main.swing.originalmenu;
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Dimension;
 import java.awt.Graphics2D;
-import java.awt.GridBagLayout;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.image.RescaleOp;
+import java.awt.image.BufferedImage;
 import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
-import java.awt.FontFormatException;
-import java.awt.image.BufferedImage;
-import java.awt.image.RescaleOp;
-import java.awt.event.KeyEvent;
-import javax.swing.JButton;
-import javax.swing.JPanel;
-import java.io.IOException;
-import java.io.File;
-import java.util.List;
 
+import java.util.List;
+import javax.swing.JPanel;
 import jsettlers.graphics.image.NullImage;
 import jsettlers.graphics.image.SingleImage;
 import jsettlers.graphics.map.draw.ImageProvider;
@@ -27,77 +21,14 @@ import jsettlers.common.images.EImageLinkType;
 import jsettlers.main.swing.JSettlersFrame;
 
 
-class CampaignMenuButton extends JButton {
-
-    public final int buttonWidth = 104;
-    public final int buttonHeight = 44;
-    public boolean hovered = false;
-    public boolean pressed = false;
-    public int offsetX;
-    public int offsetY;
-
-    public final Font textFont;
-    public final BufferedImage buttonImage;
-    public final BufferedImage buttonImageHovered;
-    public final BufferedImage buttonImagePressed;
-
-
-    public CampaignMenuButton(MenuButtonProperties properties, String buttonText, int offsetX, int offsetY) {
-
-        this.textFont = properties.buttonFont;
-        this.buttonImage = properties.buttonImage;
-        this.buttonImageHovered = properties.buttonImageHovered;
-        this.buttonImagePressed = properties.buttonImagePressed;
-
-        this.offsetX = offsetX;
-        this.offsetY = offsetY;
-        this.setText(buttonText);
-        this.setBounds(this.offsetX, this.offsetY, this.buttonWidth, this.buttonHeight);
-        this.setBorderPainted(false);
-        this.setContentAreaFilled(false);
-        this.setOpaque(false);
-        this.setForeground(new Color(248, 220, 0));
-        this.setFont(this.textFont.deriveFont(Font.PLAIN, 11f));
-
-        return;
-    }
-
-
-    @Override
-    public void paintComponent(Graphics graphics) {
-
-        if (this.pressed) {
-            graphics.drawImage(this.buttonImagePressed, 0, 0, this.getWidth(), this.getHeight(), this);
-        }
-
-        else if (this.hovered) {
-            graphics.drawImage(this.buttonImageHovered, 0, 0, this.getWidth(), this.getHeight(), this);
-        }
-
-        else {
-            graphics.drawImage(this.buttonImage, 0, 0, this.getWidth(), this.getHeight(), this);
-        }
-
-        super.paintComponent(graphics);
-
-        return;
-    }
-}
-
-
-class CampaignBackground extends JPanel {
-
-    public final BufferedImage menuImage;
-    public final JSettlersFrame mainFrame;
-    public final double idealAspectRatio = (double) 800 / (double) 600;
+class CampaignBackground extends OriginalBackgroundBase {
 
     public final JPanel buttonsPanel;
-    public final CampaignMenuButton egyptians;
-    public final CampaignMenuButton romans;
-    public final CampaignMenuButton asians;
+    public final OriginalMenuButton egyptians;
+    public final OriginalMenuButton romans;
+    public final OriginalMenuButton asians;
 
-    public Font menuFont;
-    public Font menuFontBold;
+    public final BufferedImage menuImage;
     public final BufferedImage buttonImage;
     public final BufferedImage buttonImagePressed;
     public final BufferedImage buttonImageHovered;
@@ -105,7 +36,7 @@ class CampaignBackground extends JPanel {
 
     public CampaignBackground(JSettlersFrame mainFrame) {
 
-        this.mainFrame = mainFrame;
+        super(mainFrame);
 
         // load images
         ImageProvider imageProvider = ImageProvider.getInstance();
@@ -127,34 +58,22 @@ class CampaignBackground extends JPanel {
         this.buttonImagePressed = buttonImagePressed.convertToBufferedImage();
         this.buttonImageHovered = new BufferedImage(104, 44, BufferedImage.TYPE_INT_ARGB);
 
-        // set hover effects
+        // create hovered version of menu button
         RescaleOp brightness = new RescaleOp(0.95f, 0, null);
         brightness.filter(this.buttonImage, this.buttonImageHovered);
 
-        // load font
-        File fontPath = new File("D:\\ms-sans-serif-1.ttf");
-        File fontPathBold = new File("D:\\ms-sans-serif-bold.ttf");
-
-        try {
-            this.menuFont = Font.createFont(Font.TRUETYPE_FONT, fontPath);
-            this.menuFontBold = Font.createFont(Font.TRUETYPE_FONT, fontPathBold);
-        }
-
-        catch (IOException | FontFormatException exception) {
-
-            this.menuFont = new Font("Arial", Font.PLAIN, 11);
-            this.menuFontBold = new Font("Arial", Font.BOLD, 11);
-
-            System.out.printf("failed to open campaign menu font\n");
-            exception.printStackTrace();
-        }
-
         // add buttons
-        MenuButtonProperties buttonProperties = new MenuButtonProperties(this.buttonImage, this.buttonImageHovered, this.buttonImagePressed, this.menuFont);
+        ButtonProps buttonProps = new ButtonProps(
+            this.buttonImage,
+            this.buttonImageHovered,
+            this.buttonImagePressed,
+            this.menuFont, 11, true,
+            new Color(248, 220, 0)
+        );
 
-        this.egyptians = new CampaignMenuButton(buttonProperties, "Egyptians", 56, 164);
-        this.romans = new CampaignMenuButton(buttonProperties, "Romans", 240, 440);
-        this.asians = new CampaignMenuButton(buttonProperties, "Asians", 664, 200);
+        this.egyptians = new OriginalMenuButton(buttonProps, "Egyptians", 56, 164);
+        this.romans = new OriginalMenuButton(buttonProps, "Romans", 240, 440);
+        this.asians = new OriginalMenuButton(buttonProps, "Asians", 664, 200);
 
         // add button event listeners
 
@@ -175,106 +94,81 @@ class CampaignBackground extends JPanel {
 
 
     @Override
-    public Dimension getPreferredSize() {
-
-        // size needs to be based on parent size while also keeping aspect ratio
-
-        Dimension parentSize = this.getParent().getSize();
-        double currentAspectRatio = (double) parentSize.width / (double) parentSize.height;
-
-        // height becomes deciding
-        if (currentAspectRatio >= this.idealAspectRatio) {
-
-            int newViewportWidth = (int) (this.idealAspectRatio * parentSize.height);
-            int newViewportHeight = parentSize.height;
-
-            Dimension newSize = new Dimension(newViewportWidth, newViewportHeight);
-
-            return newSize;
-        }
-
-        // width becomes deciding
-        else {
-
-            int newViewportWidth = parentSize.width;
-            int newViewportHeight = (int) (parentSize.width / this.idealAspectRatio);
-
-            Dimension newSize = new Dimension(newViewportWidth, newViewportHeight);
-
-            return newSize;
-        }
-    }
-
-
-    @Override
     public void paintComponent(Graphics graphics) {
 
-        ((Graphics2D) graphics).setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        // todo: add hover effects
 
         super.paintComponent(graphics);
 
-        Graphics2D menuGraphics = this.menuImage.createGraphics();
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+        // note: tempBuffer has a fixed size of 800 x 600
+        // note: all images are painted on temp buffer which is then painted onto the main frame
+        // note: don't paint onto the menuImage directly; it will cause artifacts from text antialiasing
+        // note: text antialiasing artifacts make text look thicker on each resize
+
+        Graphics2D tempContext = this.tempBuffer.createGraphics();
+
+        tempContext.drawImage(this.menuImage, 0, 0, this.tempBuffer.getWidth(), this.tempBuffer.getHeight(), this);
 
         // paint buttons
-        this.buttonsPanel.printAll(menuGraphics);
+        this.buttonsPanel.printAll(tempContext);
 
         // paint text
-        menuGraphics.setColor(new Color(248, 220, 0));
-        menuGraphics.setFont(this.menuFont.deriveFont(11f));
+        tempContext.setColor(new Color(248, 220, 0));
+        tempContext.setFont(this.menuFont.deriveFont(11f));
 
-        menuGraphics.drawString("Ramadamses (hard)", 125, 367);
-        menuGraphics.drawString("Septimus Marius (easy)", 342, 515);
-        menuGraphics.drawString("Tsu Tang (medium)", 602, 367);
+        tempContext.drawString("Ramadamses (hard)", 125, 367);
+        tempContext.drawString("Septimus Marius (easy)", 342, 515);
+        tempContext.drawString("Tsu Tang (medium)", 602, 367);
 
-        try {
-            this.menuFontBold = Font.createFont(Font.TRUETYPE_FONT, new File("D:\\ms-sans-serif-bold.ttf"));
-        }
-
-        catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        menuGraphics.setColor(new Color(248, 92, 24));
-
-        // test font sizes
-        /**
-        for (int item = 0; item < 6; item += 1) {
-            menuGraphics.setFont(this.menuFontBold.deriveFont(Font.BOLD, (float) (11 + item)));
-            // menuGraphics.drawString("Choose a Race", 352, 80 + item * 20);
-            menuGraphics.drawString("Game Setup", 352, 80 + item * 20);
-
-            menuGraphics.setFont(this.menuFontBold.deriveFont((float) (11 + item)));
-            // menuGraphics.drawString("Choose a Race", 500, 80 + item * 20);
-            menuGraphics.drawString("Game Setup", 500, 80 + item * 20);
-        }
-        */
+        // draw title
+        // note: only use antialiasing for larger text
+        // note: antialiasing on smaller fonts make them unreadable but is necessary for larger fonts
+        tempContext.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);  // HRGB looks good
 
         String title = "Choose a Race";
         int[] letterXOffsets = {0, 10, 18, 27, 36, 44, 53, 57, 66, 70, 81, 90, 98};
 
+        // note: this loop is necessary for proper letter spacing
+
+        // draw title shadow
+        tempContext.setColor(Color.BLACK);
+
         for (int index = 0; index < title.length(); index += 1) {
-            menuGraphics.setFont(this.menuFontBold.deriveFont(14.00f));
-            menuGraphics.drawString(String.format("%c", title.charAt(index)), 350 + letterXOffsets[index], 53);  // note: bold is not working
+            tempContext.setFont(this.menuFont.deriveFont(Font.BOLD, 14.00f));
+            tempContext.drawString(String.format("%c", title.charAt(index)), 351 + letterXOffsets[index], 54);
         }
 
-        menuGraphics.dispose();
+        // draw title text
+        tempContext.setColor(new Color(248, 92, 24));
 
-        graphics.drawImage(this.menuImage, 0, 0, this.getWidth(), this.getHeight(), this);
+        for (int index = 0; index < title.length(); index += 1) {
+            tempContext.setFont(this.menuFont.deriveFont(Font.BOLD, 14.00f));
+            tempContext.drawString(String.format("%c", title.charAt(index)), 350 + letterXOffsets[index], 53);
+        }
+
+        tempContext.dispose();
+
+        ((Graphics2D) graphics).setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+        graphics.drawImage(this.tempBuffer, 0, 0, this.getWidth(), this.getHeight(), this);
 
         return;
     }
 }
 
 
-public class OriginalCampaignMenu extends JPanel {
+public class OriginalCampaignMenu extends OriginalMenuBase {
 
-    public final JSettlersFrame mainFrame;
     public final KeyEventDispatcher campaignMenuKeyListener;
 
 
     public OriginalCampaignMenu(JSettlersFrame mainFrame) {
 
-        this.mainFrame = mainFrame;
+        super(mainFrame);
+
         this.campaignMenuKeyListener = (event) -> {
 
             if (event.getID() == KeyEvent.KEY_PRESSED && event.getKeyCode() == KeyEvent.VK_ESCAPE) {
@@ -284,13 +178,7 @@ public class OriginalCampaignMenu extends JPanel {
             return true;
         };
 
-        this.setOpaque(true);
-        this.setBackground(Color.BLACK);
-        this.setLayout(new GridBagLayout());
-        this.setMinimumSize(new Dimension(800, 600));
-
         CampaignBackground background = new CampaignBackground(this.mainFrame);
-
         this.add(background);
 
         return;
@@ -305,12 +193,5 @@ public class OriginalCampaignMenu extends JPanel {
         this.mainFrame.showOriginalMainMenu();
 
         return;
-    }
-
-
-    @Override
-    public Dimension getPreferredSize() {
-        Dimension preferredSize = this.getMinimumSize();
-        return preferredSize;
     }
 }

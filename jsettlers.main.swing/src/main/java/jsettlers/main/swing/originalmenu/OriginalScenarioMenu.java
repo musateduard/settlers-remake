123f671514c0194bea553a18a432af8158458bb5
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-09-13 21:14:38 +0200

    added jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/OriginalScenarioMenu.java

---
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/OriginalScenarioMenu.java
@@ -0,0 +1,17 @@
+package jsettlers.main.swing.originalmenu;
+
+import jsettlers.main.swing.JSettlersFrame;
+
+
+public class OriginalScenarioMenu extends OriginalMenuBase {
+
+    public OriginalScenarioMenu(JSettlersFrame mainFrame) {
+
+        super(mainFrame);
+        // no key listener
+
+        // add background
+
+        return;
+    }
+}

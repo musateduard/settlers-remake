3f1bda5a4896f0e0e513ffdd921ec82614a3298a
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-09-14 03:18:58 +0200

    updated OriginalScenarioMenu.java

--- jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/OriginalScenarioMenu.java
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/OriginalScenarioMenu.java
@@ -1,8 +1,21 @@
 package jsettlers.main.swing.originalmenu;
 
+import jsettlers.common.images.EImageLinkType;
+import jsettlers.common.images.OriginalImageLink;
+import jsettlers.graphics.image.NullImage;
+import jsettlers.graphics.image.SingleImage;
+import jsettlers.graphics.map.draw.ImageProvider;
 import jsettlers.main.swing.JSettlersFrame;
 
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class OriginalScenarioMenu extends JPanel {
@@ -16,8 +29,72 @@ public class OriginalScenarioMenu extends JPanel {
 
         // no key listener
 
-        // add background
+        // load all images
+        ImageProvider imageProvider = ImageProvider.getInstance();
+
+        SingleImage menuImage = (SingleImage) imageProvider.getImage(new OriginalImageLink(EImageLinkType.GUI, 2, 13));
+        SingleImage buttonImage = (SingleImage) imageProvider.getImage(new OriginalImageLink(EImageLinkType.SETTLER, 2, 1, 0));
+        SingleImage buttonImagePressed = (SingleImage) imageProvider.getImage(new OriginalImageLink(EImageLinkType.SETTLER, 2, 1, 1));
+        BufferedImage buttonImageHovered = new BufferedImage(buttonImagePressed.getWidth(), buttonImagePressed.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        assert menuImage instanceof NullImage == false;
+        assert buttonImage instanceof NullImage == false;
+        assert buttonImagePressed instanceof NullImage == false;
+
+        RescaleOp brightness = new RescaleOp(1.10f, 0, null);
+        brightness.filter(buttonImage.convertToBufferedImage(), buttonImageHovered);
+
+        // load all fonts
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream fontStream = loader.getResourceAsStream("ms-sans-serif-1.ttf");
+        InputStream fontBoldStream = loader.getResourceAsStream("ms-sans-serif-bold.ttf");
+
+        assert fontStream != null;
+        assert fontBoldStream != null;
+
+        Font menuFont;
+        Font menuFontBold;
+
+        try {
+            menuFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+            menuFontBold = Font.createFont(Font.TRUETYPE_FONT, fontBoldStream);
+        }
+
+        catch (IOException | FontFormatException exception) {
+
+            menuFont = new Font("Arial", Font.PLAIN, 11);
+            menuFontBold = new Font("Arial", Font.BOLD, 14);
+
+            System.out.printf("failed to open menu font: %s\n", loader.getName());
+            exception.printStackTrace();
+        }
+
+        // declare all buttons and event listeners
+        ButtonProps buttonProps = new ButtonProps(
+            buttonImage.convertToBufferedImage(),
+            buttonImageHovered,
+            buttonImagePressed.convertToBufferedImage(),
+            menuFont, 11, true,
+            new Color(248, 220, 0)
+        );
+
+        OriginalMenuButton[] buttonList = {
+            new OriginalMenuButton(buttonProps, "Cancel", 561, 583),
+            new OriginalMenuButton(buttonProps, "OK", 681, 583)
+        };
+
+        // todo: add event listeners
+
+        // declare all text
+
 
         return;
+    }
+
+
+    @Override
+    public Dimension getPreferredSize() {
+        Dimension preferredSize = this.getMinimumSize();
+        return preferredSize;
     }
 }

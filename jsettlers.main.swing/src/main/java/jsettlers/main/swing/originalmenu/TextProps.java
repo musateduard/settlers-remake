0d507eed10f9862c9c7ad2eaa028433aeeff9e31
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-09-14 02:59:34 +0200

    added jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/TextProps.java

---
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/TextProps.java
@@ -0,0 +1,12 @@
+package jsettlers.main.swing.originalmenu;
+
+import java.awt.Font;
+import java.awt.Color;
+
+
+public record TextProps(
+    Font textFont,
+    Color textColor,
+    int shadowX,
+    int shadowY
+) {}

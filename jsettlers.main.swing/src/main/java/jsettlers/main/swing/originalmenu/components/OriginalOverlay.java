1d90918375bc5a85b49703dda95256826b8adb80
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-09-20 22:43:39 +0200

    updated OriginalOverlay

--- jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/components/OriginalOverlay.java
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/components/OriginalOverlay.java
@@ -1,20 +1,20 @@
 package jsettlers.main.swing.originalmenu.components;
 
-import javax.swing.JPanel;
-import javax.swing.JComponent;
+import java.awt.Point;
 import java.awt.Component;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
 
 public class OriginalOverlay extends JPanel implements MouseListener {
 
-    public OriginalOverlay(JComponent overlayItem) {
+    public OriginalOverlay() {
 
         this.setLayout(null);
         this.setBounds(0, 0, 800, 600);
         this.setOpaque(false);
-        this.add(overlayItem);
 
         this.addMouseListener(this);
 
@@ -25,12 +25,28 @@ public class OriginalOverlay extends JPanel implements MouseListener {
     @Override
     public void mousePressed(MouseEvent event) {
 
-        System.out.printf("mouse pressed on overlay\n");
+        Point cursor = new Point(event.getX(), event.getY());
+        Component component = this.getComponentAt(cursor);
 
-        Component[] comp = this.getComponents();
+        // dispatch event to component
+        if (component != this) {
 
-        for (Component item : comp) {
-            System.out.printf("%s\n", item);
+            int eventType = MouseEvent.MOUSE_PRESSED;
+            int offsetX = cursor.x - component.getX();
+            int offsetY = cursor.y - component.getY();
+            long when = event.getWhen();
+            int modifiers = event.getModifiersEx();
+            int count = event.getClickCount();
+            boolean popup = event.isPopupTrigger();
+
+            MouseEvent newEvent = new MouseEvent(component, eventType, when, modifiers, offsetX, offsetY, count, popup);
+            component.dispatchEvent(newEvent);
+        }
+
+        else {
+            JLayeredPane internalPanel = (JLayeredPane) this.getParent();
+            internalPanel.remove(this);
+            internalPanel.repaint();
         }
 
         return;

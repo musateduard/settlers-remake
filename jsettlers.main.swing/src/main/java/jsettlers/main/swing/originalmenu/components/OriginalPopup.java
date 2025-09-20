777f15aec019bc51bf372ae4ae446d174bd143f3
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-09-20 22:43:52 +0200

    updated OriginalPopup

--- jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/components/OriginalPopup.java
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/components/OriginalPopup.java
@@ -1,25 +1,34 @@
 package jsettlers.main.swing.originalmenu.components;
 
 import java.awt.Color;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import javax.swing.ComboBoxModel;
+import javax.swing.JLayeredPane;
 import javax.swing.JList;
 
 
 public class OriginalPopup extends JList<String> implements MouseListener {
 
+    public final OriginalDropdownList dropdown;
+
+
     public OriginalPopup(OriginalDropdownList parent) {
 
         super(parent.optionList);
 
-        ComboBoxModel<String> model = parent.getModel();
+        this.dropdown = parent;
+
+        int offsetX = this.dropdown.getX();
+        int offsetY = this.dropdown.getY() + this.dropdown.getHeight() + 8;
+        int width = this.dropdown.getWidth();
+        int height = this.getCellBounds(0, 0).height * this.dropdown.getModel().getSize();
 
         this.setOpaque(false);
-        this.setFont(parent.getFont());
+        this.setFont(this.dropdown.getFont());
         this.setBackground(Color.BLACK);
-        this.setForeground(parent.getForeground());
-        this.setBounds(parent.getX(), parent.getY() + parent.getHeight() + 8, parent.getWidth(), this.getCellBounds(0, 0).height * model.getSize());
+        this.setForeground(this.dropdown.getForeground());
+        this.setBounds(offsetX, offsetY, width, height);
 
         this.addMouseListener(this);
 
@@ -28,20 +37,34 @@ public class OriginalPopup extends JList<String> implements MouseListener {
 
 
     @Override
-    public void mouseClicked(MouseEvent event) {
-        return;
-    }
-
-
-    @Override
     public void mousePressed(MouseEvent event) {
-        System.out.printf("event received on popup\n");
+
+        // get list item at cursor
+        Point cursor = event.getPoint();
+        JLayeredPane internalPanel = (JLayeredPane) this.getParent().getParent();
+        OriginalOverlay overlay = (OriginalOverlay) this.getParent();
+        int index = this.locationToIndex(cursor);
+
+        // set dropdown to selected item
+        this.dropdown.setSelectedIndex(index);
+
+        // close overlay
+        internalPanel.remove(overlay);
+        internalPanel.revalidate();
+        internalPanel.repaint();
+
         return;
     }
 
 
     @Override
     public void mouseReleased(MouseEvent event) {
+        return;
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
         return;
     }
 

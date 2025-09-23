0a697ccc0975d93433f1ce8182d6eb4622e77c0d
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-09-23 02:00:54 +0200

    updated OriginalDialog.java

---
+++ jsettlers.main.swing/src/main/java/jsettlers/main/swing/originalmenu/components/OriginalDialog.java
@@ -0,0 +1,86 @@
+package jsettlers.main.swing.originalmenu.components;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+
+public class OriginalDialog extends JPanel implements MouseListener {
+
+    public final BufferedImage dialogImage;
+
+
+    public OriginalDialog(BufferedImage dialogImage, OriginalButton[] buttonList, OriginalDropdown[] dropdownList) {
+
+        super();
+
+        this.dialogImage = dialogImage;
+
+        this.setLayout(null);
+        this.setBounds(100, 100, 600, 400);
+        // this.setBackground(Color.CYAN);
+        this.setOpaque(true);
+
+        if (buttonList != null) {
+
+            for (OriginalButton button : buttonList) {
+                this.add(button);
+            }
+        }
+
+        // load image
+        // add elements to dialog window
+        // add dropdown list popups to overlay on top of dialog layer
+
+        // image index at file_2::menu_11
+
+        this.addMouseListener(this);
+
+        return;
+    }
+
+
+    @Override
+    public void paintComponent(Graphics graphics) {
+
+        super.paintComponent(graphics);
+
+        graphics.drawImage(this.dialogImage, 0, 0, this.getWidth(), this.getHeight(), this);
+
+        return;
+    }
+
+
+    @Override
+    public void mousePressed(MouseEvent event) {
+        System.out.printf("dialog received mouse press\n");
+        return;
+    }
+
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+        return;
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        return;
+    }
+
+
+    @Override
+    public void mouseEntered(MouseEvent event) {
+        return;
+    }
+
+
+    @Override
+    public void mouseExited(MouseEvent event) {
+        return;
+    }
+}

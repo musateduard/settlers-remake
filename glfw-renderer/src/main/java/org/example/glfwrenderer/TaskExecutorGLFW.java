79f6978e4557bd800bae559aa3458b8eb5c393f5
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-10-16 03:14:08 +0200

    added TaskExecutorGLFW.java

---
+++ glfw-renderer/src/main/java/org/example/glfwrenderer/TaskExecutorGLFW.java
@@ -0,0 +1,19 @@
+package org.example.glfwrenderer;
+
+import jsettlers.network.client.task.packets.TaskPacket;
+import jsettlers.network.synchronic.timer.ITaskExecutor;
+
+
+public class TaskExecutorGLFW implements ITaskExecutor {
+
+    public TaskExecutorGLFW() {
+        return;
+    }
+
+
+    @Override
+    public void executeTask(TaskPacket task) {
+        System.out.printf("executing task object: %s\n", task);
+        return;
+    }
+}

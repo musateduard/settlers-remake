0598af07c3ea5844b7a4552b2ca3abcaf2cbedb1
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-10-19 03:20:45 +0200

    updated GameLauncherGLFW

--- glfw-renderer/src/main/java/org/example/glfwrenderer/GameLauncherGLFW.java
+++ glfw-renderer/src/main/java/org/example/glfwrenderer/GameLauncherGLFW.java
@@ -5,7 +5,7 @@ public class GameLauncherGLFW {
 
     public static void main(String[] args) throws Exception {
 
-        DemoGLFWFrame settlersGame = new DemoGLFWFrame();
+        MainWindowGLFW settlersGame = new MainWindowGLFW();
 
         settlersGame.start();
         settlersGame.cleanup();

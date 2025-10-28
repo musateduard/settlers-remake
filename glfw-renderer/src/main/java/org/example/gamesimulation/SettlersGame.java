e3261cb77175b42d6607037517e823335a756c1f
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-10-28 04:38:35 +0100

    updated SettlersGame.java

--- glfw-renderer/src/main/java/org/example/gamesimulation/SettlersGame.java
+++ glfw-renderer/src/main/java/org/example/gamesimulation/SettlersGame.java
@@ -8,14 +8,14 @@ public class SettlersGame implements Runnable {
     public static final int LOCKSTEP_DURATION_MS = 100;
     public static final int TIME_SLICE_MS = 50;
 
-    public SettlersMap gameMap;
     public boolean running;
-    public long accumulatedTime;
-    public long currentLockstep;
-    public long lastTime;
-    public long currentTime;
-    public long nextTime;
     public boolean inputReady;
+    public long accumulatedTime;
+    public long nextLoopTime;
+    public long currentLockstep;
+    public long lastLockstep;
+
+    public SettlersMap gameMap;
 
 
     public SettlersGame(SettlersMap gameMap) {
@@ -23,20 +23,29 @@ public class SettlersGame implements Runnable {
         /*
         todo: implement deterministic lockstep simulation
         todo: implement state buffer so that game thread can write to a state and render thread can read from a state
+        todo: implement circular buffer for specific lockstep with all tasks gathered in that slot
 
         note: state in this context means the map and all entities in the game world
         note: swap buffers when new state is available
         note: should we still use objects for game state or just use entities?
         */
 
+        /*
+        todo: this class needs a way to receive task packets
+        todo: we also need a way to generate tasks based on user input
+
+        task packets are synchronization packets that are received for every lockstep
+        they are required to signal the game that the simulation has advanced to that particular lockstep
+        task packets may or may not contain tasks but they are required for advancing the simulation
+        */
+
         this.gameMap = gameMap;
         this.running = true;
-        this.accumulatedTime = 0;
-        this.currentLockstep = 0;
-        this.currentTime = 0;
-        this.lastTime = 0;
-        this.nextTime = 0;
         this.inputReady = false;
+        this.accumulatedTime = 0;
+        this.nextLoopTime = 0;
+        this.currentLockstep = 0;
+        this.lastLockstep = 0;
 
         /*
         game architecture
@@ -70,9 +79,26 @@ public class SettlersGame implements Runnable {
     }
 
 
+    public void pollPackets() {
+
+        /*
+        poll network for task packets
+        if packets are available then generate task packet and advance maxAllowedLockstep
+        otherwise wait for next packet
+
+        note:
+
+        a task packet is needed regardless of user input in order to advance lockstep
+        in networked play we also need all current player task packets to arrive before advancing the lockstep
+        */
+
+        return;
+    }
+
+
     public void executeTasks() {
 
-        System.out.printf("executing tasks\n");
+        System.out.printf("executing tasks for lockstep %d\n", this.currentLockstep);
 
         /*
         for item in task_queue:
@@ -91,20 +117,30 @@ public class SettlersGame implements Runnable {
     @Override
     public void run() {
 
+        // initialize nextLoopTime to current time so that inside the loop it's always scheduled at +50ms
+        this.nextLoopTime = System.currentTimeMillis();
+
         while (this.running) {
 
-            this.currentTime = System.currentTimeMillis();
+            this.nextLoopTime += SettlersGame.TIME_SLICE_MS;
             this.currentLockstep = this.accumulatedTime / SettlersGame.LOCKSTEP_DURATION_MS;
 
-            // reschedule loop in the future and execute tasks now
-            if (this.nextTime <= this.currentTime) {
-                this.nextTime = this.currentTime + SettlersGame.TIME_SLICE_MS;
+            System.out.printf("current lockstep: %d\n", this.currentLockstep);
+            System.out.printf("next: %d\n", this.nextLoopTime);
+
+            this.pollPackets();
+
+            if (this.currentLockstep > this.lastLockstep) {
+                this.executeTasks();
             }
 
-            // wait until next loop is scheduled and then execute tasks
-            else {
+            long currentTime = System.currentTimeMillis();
+            long waitTime = this.nextLoopTime - currentTime;
+
+            // wait until next loop is scheduled
+            if (waitTime > 0) {
                 try {
-                    Thread.sleep(this.nextTime - this.currentTime);
+                    Thread.sleep(waitTime);
                 }
 
                 catch (InterruptedException exception) {
@@ -112,9 +148,17 @@ public class SettlersGame implements Runnable {
                 }
             }
 
-            this.executeTasks();
+            /*
+            adjust nextLoopTime if loop took longer than TIME_SLICE_MS to execute
+            this ensures that the game still runs in fixed 50ms intervals even
+            when a loop iteration took longer to complete
+            */
+            else if (waitTime < -SettlersGame.TIME_SLICE_MS) {
+                this.nextLoopTime = currentTime + SettlersGame.TIME_SLICE_MS;
+            }
 
             this.accumulatedTime += SettlersGame.TIME_SLICE_MS;
+            this.lastLockstep = this.currentLockstep;
             continue;
         }
 

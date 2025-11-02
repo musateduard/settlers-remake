2692cd7d87a0f1b042d895d1497fb4884b9d684c
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-11-02 22:58:46 +0100

    moved Window to separate class

---
+++ glfw-renderer/src/main/java/org/example/mainwindow/Window.java
@@ -0,0 +1,114 @@
+package org.example.mainwindow;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.system.MemoryUtil;
+
+
+public class Window {
+
+    public final int width;
+    public final int height;
+    public final long windowId;
+    public final EventManager eventManager;
+
+
+    public Window(EventManager eventManager) {
+
+        this.width = 800;
+        this.height = 600;
+        this.eventManager = eventManager;
+
+        // init glfw
+        if (!GLFW.glfwInit()) {
+            throw new IllegalStateException("Failed to initialize GLFW.");
+        }
+
+        // set window hints
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_FALSE);
+
+        // create window
+        this.windowId = GLFW.glfwCreateWindow(this.width, this.height, "demo window", MemoryUtil.NULL, MemoryUtil.NULL);
+
+        if (this.windowId == MemoryUtil.NULL) {
+            throw new RuntimeException("Failed to create the GLFW window.");
+        }
+
+        // make context current
+        GLFW.glfwMakeContextCurrent(this.windowId);
+
+        // enable vsync
+        GLFW.glfwSwapInterval(1);
+
+        // register keyboard handler
+        GLFW.glfwSetKeyCallback(
+
+            this.windowId,
+
+            (long windowId, int key, int scanCode, int action, int modifier) -> {
+
+                KeyEvent event = new KeyEvent(windowId, key, scanCode, action, modifier);
+                this.eventManager.emitKeyEvent(event);
+
+                return;
+            }
+        );
+
+        GLFW.glfwSetCursorPosCallback(
+
+            this.windowId,
+
+            (long window, double xpos, double ypos) -> {
+
+                CursorEvent event = new CursorEvent(window, xpos, ypos);
+                this.eventManager.emitCursorEvent(event);
+
+                return;
+            }
+        );
+
+        GLFW.glfwSetMouseButtonCallback(
+
+            this.windowId,
+
+            (long window, int button, int action, int mods) -> {
+
+                MouseEvent event = new MouseEvent(window, button, action, mods);
+                this.eventManager.emitMouseEvent(event);
+
+                return;
+            }
+        );
+
+        // make window visible
+        GLFW.glfwShowWindow(this.windowId);
+
+        return;
+    }
+
+
+    public boolean shouldClose() {
+        boolean shouldClose = GLFW.glfwWindowShouldClose(this.windowId);
+        return shouldClose;
+    }
+
+
+    public void cleanup() {
+        GLFW.glfwDestroyWindow(this.windowId);
+        GLFW.glfwTerminate();
+        return;
+    }
+
+
+    public void swapBuffers() {
+        GLFW.glfwSwapBuffers(this.windowId);
+        return;
+    }
+
+
+    public void pollEvents() {
+        GLFW.glfwPollEvents();
+        return;
+    }
+}

058d62a5bbcbe328a56d989cdece96529013c070
Musat Eduard <144883995+musateduard@users.noreply.github.com>
2025-11-05 03:47:17 +0100

    added GuiRenderer.java

---
+++ glfw-renderer/src/main/java/org/example/mainwindow/GuiRenderer.java
@@ -0,0 +1,127 @@
+package org.example.mainwindow;
+
+import imgui.ImGui;
+import imgui.ImFont;
+import imgui.ImGuiIO;
+import imgui.ImGuiStyle;
+import imgui.flag.ImGuiCol;
+import imgui.flag.ImGuiCond;
+import imgui.gl3.ImGuiImplGl3;
+import imgui.glfw.ImGuiImplGlfw;
+import org.lwjgl.system.MemoryUtil;
+
+import static imgui.flag.ImGuiWindowFlags.NoMove;
+import static imgui.flag.ImGuiWindowFlags.NoResize;
+import static imgui.flag.ImGuiWindowFlags.NoCollapse;
+import static imgui.flag.ImGuiWindowFlags.NoTitleBar;
+
+
+public class GuiRenderer {
+
+    public final ImGuiImplGlfw imguiGlfw;
+    public final ImGuiImplGl3 imguiOpengl;
+    public final ImFont menuFont;
+
+
+    public GuiRenderer(Window window, Renderer renderer) {
+
+        if (window.windowId == MemoryUtil.NULL) {
+            throw new RuntimeException("cannot initialize hui renderer before creating glfw window");
+        }
+
+        // create imgui context
+        ImGui.createContext();
+
+        // disable ini file
+        ImGuiIO imguiSettings = ImGui.getIO();
+
+        imguiSettings.setIniFilename(null);
+        imguiSettings.setLogFilename(null);
+
+        // load font files
+        this.menuFont = ImGui.getIO().getFonts().addFontFromFileTTF("D:\\fonts\\ms-sans-serif-1.ttf", 12.00f);
+
+        // init all backends
+        this.imguiGlfw = new ImGuiImplGlfw();
+        this.imguiOpengl = new ImGuiImplGl3();
+
+        this.imguiGlfw.init(window.windowId, true);
+        this.imguiOpengl.init(renderer.glslVersion);
+
+        return;
+    }
+
+
+    public void renderGuiStack() {
+
+        this.imguiGlfw.newFrame();
+        this.imguiOpengl.newFrame();
+        ImGui.newFrame();
+
+        // set window transparency
+        ImGuiStyle style = ImGui.getStyle();
+
+        style.setColor(ImGuiCol.WindowBg, 0, 0, 0, 50);
+        style.setColor(ImGuiCol.Button,        144, 128, 56, 255);
+        style.setColor(ImGuiCol.ButtonActive,  132, 117, 49, 255);
+        style.setColor(ImGuiCol.ButtonHovered, 138, 123, 52, 255);
+
+        // set text color
+        style.setColor(ImGuiCol.Text, 0, 12, 64, 255);
+
+        // set window size and position
+        ImGui.setNextWindowPos(0, 0, ImGuiCond.Always);
+        ImGui.setNextWindowSize(800, 600, ImGuiCond.Always);
+
+        // draw window
+        ImGui.pushFont(this.menuFont);  // set window font
+        // ImGui.begin("main menu window", NoBackground | NoDecoration);  // for release
+        ImGui.begin("main menu window", NoCollapse | NoTitleBar | NoResize | NoMove);  // for debug
+
+        // draw buttons
+        ImGui.setCursorPos(80, 20); boolean tutorial = ImGui.button("Tutorial", 172, 32);
+        ImGui.setCursorPos(80, 60); boolean campaign = ImGui.button("Campaign", 172, 32);
+        ImGui.setCursorPos(80, 100); boolean missionCdCampaign = ImGui.button("Mission CD Campaign", 172, 32);
+        ImGui.setCursorPos(80, 140); boolean amazonCampaign = ImGui.button("Amazon Campaign", 172, 32);
+        ImGui.setCursorPos(80, 180); boolean campaignNormal = ImGui.button("Campaign: Normal", 172, 32);
+        ImGui.setCursorPos(80, 220); boolean singlePlayerScenario = ImGui.button("Single Player: Scenario", 172, 32);
+        ImGui.setCursorPos(80, 260); boolean multiplayerLan = ImGui.button("Multi-player Game: LAN", 172, 32);
+        ImGui.setCursorPos(80, 300); boolean multiplayerInternet = ImGui.button("Multi-player Game: Internet", 172, 32);
+        ImGui.setCursorPos(80, 340); boolean loadGame = ImGui.button("Load Game", 172, 32);
+        ImGui.setCursorPos(80, 400); boolean onlineHelp = ImGui.button("Online Help", 172, 32);
+        ImGui.setCursorPos(80, 440); boolean tipsTricks = ImGui.button("Tips & Tricks", 172, 32);
+        ImGui.setCursorPos(80, 480); boolean credits = ImGui.button("Credits", 172, 32);
+        ImGui.setCursorPos(80, 540); boolean exitGame = ImGui.button("Exit Game", 172, 32);
+
+        // set button actions
+        if (tutorial) {
+            System.out.printf("tutorial clicked\n");
+        }
+
+        if (campaign) {
+            System.out.printf("campaign clicked\n");
+        }
+
+        // draw text labels
+        ImGui.setCursorPos(34, 577); ImGui.textColored(248, 220, 0, 255, "Version 1.60");
+
+        ImGui.end();
+        ImGui.popFont();  // pop font
+
+        ImGui.render();
+        this.imguiOpengl.renderDrawData(ImGui.getDrawData());
+
+        return;
+    }
+
+
+    public void cleanup() {
+
+        this.imguiOpengl.shutdown();
+        this.imguiGlfw.shutdown();
+
+        ImGui.destroyContext();
+
+        return;
+    }
+}
